461c926e49b55bc21d05ffe1323e643a1f374890
Ous56L <osama2262k@gmail.com>
2024-11-21 08:38:42 +0300

    Props commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,17 @@
-import Button from "./Button";
+import Student from "./Student.jsx";
+
 
 function App(){
 
-  return(<Button/>);
+  return(
+    <>
+    <Student name="Sonic" age={18} isStudent={true}/>
+    <Student name="Shadow" age={20} isStudent={false}/>
+    <Student name="Tails" age={17} isStudent={true}/>
+    <Student name="Silver" age={18} isStudent={false}/>
+    <Student/>
+    </>
+  );
 }
 
 export default App
