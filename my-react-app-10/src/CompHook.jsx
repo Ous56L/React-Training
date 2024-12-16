565c2885c6d5ddeac0328db6699b623172124f1d
Ous56L <osama2262k@gmail.com>
2024-12-16 20:40:25 +0300

    width & Height Hook Commit

--- my-react-app-10/src/CompHook.jsx
+++ my-react-app-10/src/CompHook.jsx
@@ -2,30 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 function CompHook() {
 
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState("green");
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
 
     useEffect(() => {
-        document.title = `Count: ${count} ${color}`;
-    }, [count, color]);
+        window.addEventListener("resize", handleResize);
+        console.log("EVENT LISTENER ADDED");
 
-    function addCount(){
-        setCount(c => c + 1);
+        return() => {
+            window.removeEventListener("resize", handleResize);
+            console.log("EVENT LISTENER REMOVED");
+        }
+    },[])
+
+    useEffect(() => {
+        document.title = `Size ${width} x ${height}`;
+    },[width, height])
+
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
     }
 
-    function subtractCount(){
-        setCount(c => c - 1);
-    }
-
-    function changeColor(){
-        setColor(c => c === "green" ? "red" : "green");
-    }
-
-    return (<>
-        <p style={{color: color}}>Count: {count}</p>
-        <button onClick={addCount}>Add</button> 
-        <button onClick={subtractCount}>Subtract</button><br/>
-        <button onClick={changeColor}>Change Color</button>              
-                </>);
+    return(<>
+            <p>Window Width: {width}</p>
+            <p>Window Height: {height}</p>
+            </>);
 }
 export default CompHook;
